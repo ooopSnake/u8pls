c8a6d9c53b8f70cf48e7ab6bef59964211256a84
suzhen <suzhen@guestin.cn>
2023-03-16 22:15:05 +0800

    add help

--- src/cmd.rs
+++ src/cmd.rs
@@ -13,17 +13,17 @@ pub struct ScanArgs {
 
 #[derive(Subcommand, Clone)]
 pub enum Expr {
+    /// match file suffix like: ".txt"
     Suffix {
-        /// match file suffix like: ".txt"
         suffix: String
     },
+    /// match file prefix eg:
+    /// "data_" will match: data_1.docx,data_2.bin
     Prefix {
-        /// match file prefix eg:
-        /// "data_" will match: data_1.docx,data_2.bin
         prefix: String
     },
+    /// advance match pattern eg: `202\d_[a-z]{3}\.txt` match `2023_abc.txt`
     Regexp {
-        /// regexp
         regexp: String
     },
 }
